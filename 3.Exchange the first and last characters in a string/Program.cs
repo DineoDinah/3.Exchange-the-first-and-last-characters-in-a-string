1c477941160de84e9826a4a3c126e779fb449d86
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 13:45:09 +0200

    Add project files.

---
+++ 3.Exchange the first and last characters in a string/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Exchange_the_first_and_last_characters_in_a_string
+{
+    internal class Program
+    {
+        public static string test(string str)
+        {
+            return str.Length > 1
+                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test("abcd"));
+            Console.WriteLine(test("a"));
+            Console.WriteLine(test("xy"));
+            Console.ReadLine();
+        }
+    }
+}
